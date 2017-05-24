7230a343c09ddb4b75b1bb6cc51b0e50a8a9222c
ceciliakan <hyk13@dyn111-220.ib.ic.ac.uk>
2017-05-24 18:48:36 +0100

    in progress: debug BaxterNet

--- BaxterNet.lua
+++ BaxterNet.lua
@@ -30,6 +30,7 @@ function Body:createBody()
 	imageNet:add(nn.ELU(ELU_convg))
 	imageNet:add(nn.SpatialConvolution(numFilters, numFilters, 3, 3))
 	imageNet:add(nn.ELU(ELU_convg))
+	imageNet:add(nn.PrintSize("imageNet"))
 	    
 	local depthNet = nn.Sequential()
 	depthNet:add(nn.Narrow(3, 5, 1)) -- Extract 5th channels
@@ -38,10 +39,11 @@ function Body:createBody()
 	depthNet:add(nn.ELU(ELU_convg))
 	depthNet:add(nn.SpatialConvolution(numFilters, numFilters, 3, 3))
 	depthNet:add(nn.ELU(ELU_convg))
+	depthNet:add(nn.PrintSize("depthNet"))
 	
 	local branches = nn.ConcatTable() -- Apply each module to input
 	branches:add(imageNet)
-	branches:add(depthNet)
+	-- branches:add(depthNet)
 	
 	local RGBDnet = nn.Sequential()
 	--RGBDnet:add(nn.View(-1, numFilters, 4, size, size)) -- Always pass through as batch (not using setNumInputDims)
@@ -49,13 +51,25 @@ function Body:createBody()
 	RGBDnet:add(nn.JoinTable(3))
 	RGBDnet:add(nn.SpatialConvolution(numFilters, numFilters, 3, 3))
 	RGBDnet:add(nn.ELU(ELU_convg))
+	RGBDnet:add(nn.PrintSize("RGBDnet"))
     
 	local RGBD = RGBDnet:forward(data)
 	
 	local convOutputSizes = RGBD:size()
+	print("RGBDnet output size storage:")
+	print(convOutputSizes)
 	local convOutputDim = RGBD:nDimension()
-	convOutputSizes	= torch.cumprod(convOutputSizes) -- Last value is unrolled spatial output size
-	RGBDnet:add(nn.View(convOutputSizes[{{convOutputDim, convOutputDim}}]):setNumInputDims(3)) --unroll
+	print("RGBD output dim:")
+	print(convOutputDim)
+	convOutputSizes	= torch.cumprod(torch.FloatTensor(convOutputSizes)) -- Last value is unrolled spatial output size x batch size
+	print("dimension mul:")
+	print (convOutputSizes)
+	outputSize = convOutputSizes[{{convOutputDim, convOutputDim}}] / convOutputSizes[{{1, 1}}]
+	print("unroll size")
+	print(outputSize)
+	RGBDnet:add(nn.View(outputSize):setNumInputDims(3)) --unroll
+	
+	RGBDnet:add(nn.PrintSize("RGBDnet unrolled"))
 
 	local motorNet = nn.Sequential()
 	motorNet:add(nn.Narrow(3, 4, 1)) -- Extract 4th channel
@@ -66,11 +80,10 @@ function Body:createBody()
 	motorNet:add(nn.Linear(histLen * motorInputs, numFilters)) -- Expand to number of convolutional feature maps
 	--motorNet:add(nn.Replicate(convOutputSizes[3], 2, 1)) -- Replicate over height (begins spatial tiling)
 	--motorNet:add(nn.Replicate(convOutputSizes[4], 3, 2)) -- Replicate over width (completes spatial tiling)
+	motorNet:add(nn.PrintSize(motorNet))
 	
 	local M = motorNet:forward(data)
-	
 	local merge = nn.JoinTable(2,1):forward{RGBD,M}
-
 	--[[local net = nn.Sequential()
 	net:add(nn.View(-1, histLen, 5, size, size)) -- Always pass through as batch (not using setNumInputDims)
 	net:add(merge)
